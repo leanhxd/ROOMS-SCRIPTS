9e9390a409e808e61c05619f6905daf9f7a30599
lean <117493425+leanhxd@users.noreply.github.com>
2022-12-05 20:22:52 +0800

    Add files via upload

---
+++ infsprint/infsprint.lua
@@ -0,0 +1,24 @@
+local plr = game:GetService("Players").LocalPlayer
+local char = plr.Character or plr.CharacterAdded:Wait()
+local plrgui = plr.PlayerGui
+local stamina = plrgui.stamina
+local shifting = false
+
+stamina.LocalScript:Destroy()
+
+local UIS = game:GetService("UserInputService")
+
+local function onInputBegan(input)
+    if input.UserInputType == Enum.UserInputType.LeftShift then
+        if shifting == false then
+            shifting = true
+            char.Humanoid.WalkSpeed = 40
+        else
+            char.Humanoid.WalkSpeed = 16
+        end
+    end
+end
+
+
+    UIS.InputBegan:Connect(onInputBegan)
+
